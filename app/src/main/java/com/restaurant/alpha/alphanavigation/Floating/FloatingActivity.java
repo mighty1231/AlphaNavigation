97ea17a77358544eb30254ed463abaa3ed73adef
Seong Hoi <mighty1231@gmail.com>
2016-06-01 15:35:33 +0900

    Some packages and fragment files are updated

---
+++ app/src/main/java/com/restaurant/alpha/alphanavigation/Floating/FloatingActivity.java
@@ -0,0 +1,15 @@
+package com.restaurant.alpha.alphanavigation.Floating;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.restaurant.alpha.alphanavigation.R;
+
+public class FloatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating);
+    }
+}
---
+++ app/src/main/java/com/restaurant/alpha/alphanavigation/Main/StopsFragment.java
@@ -0,0 +1,32 @@
+package com.restaurant.alpha.alphanavigation.Main;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.restaurant.alpha.alphanavigation.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StopsFragment extends Fragment {
+
+
+    public StopsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
