4c65d5c440bd9b57d021433e9b8c0afd3a691afd
mshy123 <mshy123@kaist.ac.kr>
2016-06-05 03:47:03 +0900

    Select by Name, Select by Map, Stop, Destination Finish

--- app/src/main/java/com/restaurant/alpha/alphanavigation/SelectLocationByMapActivity.java
+++ app/src/main/java/com/restaurant/alpha/alphanavigation/SelectLocationByMapActivity.java
@@ -1,15 +1,31 @@
 package com.restaurant.alpha.alphanavigation;
 
+import android.content.Intent;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.skp.Tmap.TMapAddressInfo;
+import com.skp.Tmap.TMapData;
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapMarkerItem2;
+import com.skp.Tmap.TMapPOIItem;
+import com.skp.Tmap.TMapPoint;
+import com.skp.Tmap.TMapView;
+
+import java.util.ArrayList;
 
 public class SelectLocationByMapActivity extends AppCompatActivity {
+    private TMapView tmapview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,30 @@ public class SelectLocationByMapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_location_by_map);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RelativeLayout locationByNameRelativeLayout = (RelativeLayout)findViewById(R.id.tmapapi);
+        assert(locationByNameRelativeLayout != null);
+        this.tmapview = new TMapView(this);
+        locationByNameRelativeLayout.addView(tmapview);
+
+        this.tmapview.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback() {
+            @Override
+            public boolean onPressUpEvent(ArrayList<TMapMarkerItem> markerlist,ArrayList<TMapPOIItem> poilist, TMapPoint point, PointF pointf) {
+                TMapMarkerItem2 tMapMarkerItem2 = new TMapMarkerItem2();
+                TMapMarkerItem tMapMarkerItem = new TMapMarkerItem();
+                tMapMarkerItem2.setTMapPoint(point);
+                tMapMarkerItem.setTMapPoint(point);
+                tMapMarkerItem.setVisible(TMapMarkerItem.VISIBLE);
+                tmapview.addMarkerItem2("location", tMapMarkerItem2);
+                tmapview.addMarkerItem("location", tMapMarkerItem);
+                return true;
+            }
+            @Override
+            public boolean onPressEvent(ArrayList<TMapMarkerItem> markerlist,ArrayList<TMapPOIItem> poilist, TMapPoint point, PointF pointf) {
+                return true;
+            }
+        });
+
     }
 
     @Override
@@ -33,7 +73,33 @@ public class SelectLocationByMapActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_send) {
-            finish();
+            ArrayList<TMapMarkerItem2> tMapMarkerItem2ArrayList = this.tmapview.getAllMarkerItem2();
+
+            if (tMapMarkerItem2ArrayList.size() > 0) {
+                TMapMarkerItem2 tMapMarkerItem2 = tMapMarkerItem2ArrayList.get(0);
+                TMapPoint tMapPoint = tMapMarkerItem2.getTMapPoint();
+
+                final double[] position = new double[2];
+                position[0] = tMapPoint.getLatitude();
+                position[1] = tMapPoint.getLongitude();
+
+                TMapData tMapData = new TMapData();
+                tMapData.reverseGeocoding(position[0], position[1], "A04", new TMapData.reverseGeocodingListenerCallback() {
+                    @Override
+                    public void onReverseGeocoding(TMapAddressInfo tMapAddressInfo) {
+                        Intent intent = new Intent();
+                        intent.putExtra("pos", position);
+                        if (tMapAddressInfo.strBuildingName != null) intent.putExtra("name", tMapAddressInfo.strBuildingName);
+                        else if (tMapAddressInfo.strRoadName != null) intent.putExtra("name", tMapAddressInfo.strRoadName);
+                        else intent.putExtra("name", tMapAddressInfo.strFullAddress);
+                        setResult(BasicSettingActivity.RESULT_OK, intent);
+                        finish();
+                    }
+                });
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Choose the location", Toast.LENGTH_SHORT).show();
+            }
         }
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/restaurant/alpha/alphanavigation/SelectLocationByNameActivity.java
+++ app/src/main/java/com/restaurant/alpha/alphanavigation/SelectLocationByNameActivity.java
@@ -1,28 +1,85 @@
 package com.restaurant.alpha.alphanavigation;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.RelativeLayout;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.skp.Tmap.TMapView;
+import com.skp.Tmap.TMapData;
+import com.skp.Tmap.TMapPOIItem;
+import com.skp.Tmap.TMapPoint;
+
+import java.util.ArrayList;
 
 public class SelectLocationByNameActivity extends AppCompatActivity {
-    private RelativeLayout mMainRelativeLayout =null;
+    private final int maxSearchSize = 20;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_location_by_name);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mMainRelativeLayout =(RelativeLayout)findViewById(R.id.tmapapi);
-        TMapView tmapview = new TMapView(this);        // TmapView생성
 
-        mMainRelativeLayout.addView(tmapview);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.search_recycler_view);
+        final EditText editText = (EditText) findViewById(R.id.name_keyword);
 
-        tmapview.setSKPMapApiKey("7862e03c-f02d-3eba-a686-5a01ff03a257");
+        assert(recyclerView != null);
+        assert(editText != null);
+
+        final SearchByNameAdapter searchByNameAdapter = new SearchByNameAdapter(this);
+        recyclerView.setAdapter(searchByNameAdapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(final TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    final String keyword = v.getText().toString();
+
+                    TMapData tMapData = new TMapData();
+                    tMapData.findAllPOI(keyword, new TMapData.FindAllPOIListenerCallback() {
+                        @Override
+                        public void onFindAllPOI(final ArrayList<TMapPOIItem> arrayList) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    searchByNameAdapter.deleteAllLocation();
+                                    if (arrayList.size() != 0) {
+                                        int size = maxSearchSize;
+                                        if (arrayList.size() < maxSearchSize) size = arrayList.size();
+                                        for (int i = 0; i < size; i++) {
+                                            searchByNameAdapter.addLocation(arrayList.get(i));
+                                        }
+                                    }
+                                    else {
+                                        Toast.makeText(getApplicationContext(), "No result about " + keyword, Toast.LENGTH_SHORT).show();
+                                    }
+                                    searchByNameAdapter.notifyDataSetChanged();
+                                }
+                            });
+                        }
+                    });
+
+                    InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -38,9 +95,9 @@ public class SelectLocationByNameActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_send) {
-            finish();
-        }
+        //if (id == R.id.action_send) {
+
+        //}
         return super.onOptionsItemSelected(item);
     }
 }
